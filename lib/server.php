2f2ec9104c5033babe4d4e3cb8acf82479a8fa85
unknown <alessanderrubiato@yahoo.com>
2018-11-12 16:01:32 +0800

    first commit

---
+++ lib/server.php
@@ -0,0 +1,12 @@
+<?php
+$dbserver = 'localhost';
+$dbname = 'reward_sys'
+$dbuser = 'root';
+$dbpass = '';
+
+
+
+$conn = mysqli_connect($dbserver,$dbname,$dbuser,$dbpass) or die("Error in connecting database");
+
+
+?>
